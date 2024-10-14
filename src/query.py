246126c44c198c4b7c1c3ef66165a46f88a0f34f
Kareem Eldahshoury <eldah001@umn.edu>
2024-10-14 13:25:50 -0500

    Find time equal update

--- src/query.py
+++ src/query.py
@@ -333,7 +333,407 @@ def find_time_pyramid(
     filter_value: float,
 ):
     # TODO:
-    pass
+    ts = get_timeseries(
+        variable,
+        start_datetime,
+        end_datetime,
+        time_resolution,
+        time_agg_method,
+        min_lat,
+        max_lat,
+        min_lon,
+        max_lon,
+        time_series_aggregation_method,
+    )
+    #how do I check years here and if true how do I enter into months
+    res = []
+    if filter_predicate == ">":
+        if time_series_aggregation_method == "max":
+            for i in ts:
+                # if variable at time i is greater than max -> True, else -> False
+                if(i > filter_value):
+                    res.append(True)
+                else:
+                    res.append(False)
+        elif time_series_aggregation_method == "min":
+            for i in ts:
+                if i < filter_value:
+                    res.append(True)
+                else:
+                    res.append(False)
+    elif filter_predicate == "<":
+        res = 0
+    elif filter_predicate == ">=":
+        res = 0
+    elif filter_predicate == "<=":
+        res = 0
+    elif filter_predicate == "==":
+        res = 0
+    elif filter_predicate == "!=":
+        res = 0
+    return res
+
+def find_time_equal(
+    variable: str,
+    start_datetime: str,
+    end_datetime: str,
+    time_resolution: str,  # e.g., "hour", "day", "month", "year"
+    time_agg_method: str,  # e.g., "mean", "max", "min"
+    min_lat: float,
+    max_lat: float,
+    min_lon: float,
+    max_lon: float,
+    time_series_aggregation_method: str,  # e.g., "mean", "max", "min"
+    filter_value: float,
+):
+    # TODO:
+    """
+        For the granularities lower than years, we can get the whole year periods, then check which years should be taken out of consideration
+        Then take the years that have months that can still work then split those into months and check each months.
+        If we go lower than months we can then eliminate the months that dont fit, take the ones that do and split them into days and check.
+        And then if we want hours we eliminate the days that dont fit and split the days that do into hours and get those hours that fit.
+    """
+    xrds_list = []
+    years_passed = []
+    res = []
+    if time_resolution == 'year':
+        years, months, days, hours = get_whole_period_between(start_datetime, end_datetime)
+        if time_agg_method == 'max':
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-max.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].max() == filter_value).any():
+                    years_passed.append(y)
+        elif time_agg_method == 'min':
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-min.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() == filter_value).any():
+                    years_passed.append(y)
+            #res.append((year_data["t2m"].min()))
+        elif time_agg_method == 'mean':
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-mean.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(np.mean(year_data["t2m"]) == filter_value).any():
+                    years_passed.append(y)
+            #res.append((np.mean(year_data["t2m"])))
+        # if time_resolution == "year":
+        #     return years_passed
+    if time_resolution == "month":
+        years, months, days, hours = get_whole_period_between(start_datetime, end_datetime)
+        ds_list = []
+
+        if time_agg_method == 'max':
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-max.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-max.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    if (month_data["t2m"].max() == filter_value).any():
+                        res.append(str(m.values)[:7])
+
+            
+
+        
+        elif time_agg_method == 'min':
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-min.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-min.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+            #return years_passed #2023
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    #return month_data["t2m"].min()
+                    if (month_data["t2m"].min() == filter_value).any():
+                        res.append(str(m.values)[:7])
+
+            
+
+    elif time_resolution == "day":
+        years, months, days, hours = get_whole_period_between(start_datetime, end_datetime)
+        months_passed = []
+        ds_list = []
+        ds_m_list = []
+        if time_agg_method == "max":
+            day = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-day-max.nc")
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-max.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-max.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+            
+
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    if (month_data["t2m"].min() <= filter_value and filter_value <= month_data["t2m"].max()).any():
+                        months_passed.append(str(m.values)[:10])
+            
+            for m in months_passed:
+                ds_m = day.sel(
+                    time=slice(f"{m[:7]}-01 00:00:00", f"{m} 23:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds_m:
+                    ds_m_list.append(ds_m)
+                
+            if ds_m_list:
+                ds_m = xr.concat(ds_m_list, dim="time")
+                for d in ds_m["time"]:
+                    day_data = ds_m.sel(time=d)
+                    #return day_data["t2m"].max()
+                    if(day_data["t2m"].max() == filter_value):
+                        res.append(str(d.values)[:10])
+
+            return res
+        
+        elif time_agg_method == "min":
+            day = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-day-min.nc")
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-min.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-min.nc")
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                #return year_data["t2m"].min()
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+            
+            #return years_passed
+            
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    if (month_data["t2m"].min() <= filter_value and filter_value <= month_data["t2m"].max()).any():
+                        months_passed.append(str(m.values)[:10])
+            
+            for m in months_passed:
+                ds_m = day.sel(
+                    time=slice(f"{m[:7]}-01 00:00:00", f"{m} 23:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds_m:
+                    ds_m_list.append(ds_m)
+                
+            if ds_m_list:
+                ds_m = xr.concat(ds_m_list, dim="time")
+                for d in ds_m["time"]:
+                    day_data = ds_m.sel(time=d)
+                    # res.append(day_data["t2m"].min())
+                    if(day_data["t2m"].min() == filter_value):
+                        res.append(str(d.values)[:10])
+
+            return res
+        
+    elif time_resolution == "hour":
+        years, months, days, hours = get_whole_period_between(start_datetime, end_datetime)
+        months_passed = []
+        days_passed = []
+        ds_list = []
+        ds_m_list = []
+        ds_d_list = []
+        if time_agg_method == "max":
+            #hour = xr.open_dataset("/data/era5/raw/2_tm_temperature-")
+            day = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-day-max.nc")
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-max.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-max.nc")
+
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+            
+            #return years_passed
+
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    if (month_data["t2m"].min() <= filter_value and filter_value <= month_data["t2m"].max()).any():
+                        months_passed.append(str(m.values)[:10])
+            
+            #return months_passed
+            
+            for m in months_passed:
+                ds_m = day.sel(
+                    time=slice(f"{m[:7]}-01 00:00:00", f"{m} 23:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds_m:
+                    ds_m_list.append(ds_m)
+
+            if ds_m_list:
+                ds_m = xr.concat(ds_m_list, dim="time")
+                for d in ds_m["time"]:
+                    day_data = ds_m.sel(time=d)
+                    #return day_data["t2m"].max()
+                    if(day_data["t2m"].max() >= filter_value and filter_value >= day_data["t2m"].min()):
+                        days_passed.append(str(d.values)[:10])
+            
+            #return days_passed
+            
+            for d in days_passed:
+                whole_year, whole_month, whole_day, ds_d = get_whole_period_between(f"{d} 00:00:00", f"{d} 23:00:00")
+
+                if ds_d:
+                    ds_d_list.append(ds_d)
+            #return ds_d_list
+ 
+            for h in ds_d_list:
+                #return h[0][:4]
+                hour_dataset = xr.open_dataset(f"{RAW_DATA_PATH}/{variable}/{variable}-{h[0][:4]}.nc")
+                for hour in h:
+                    #return h
+                    #return hour[:4]
+                    temp = hour_dataset["t2m"].sel(time=hour).values
+                    #return temp.max() #317.20237690953525
+                    #hour_temp_list.append((hour, temp.max()))
+                    #return hour_temp_list
+                    if(temp.max() == filter_value):
+                        res.append(hour)
+
+            return res
+        elif time_agg_method == "min":
+            #hour = xr.open_dataset("/data/era5/raw/2_tm_temperature-")
+            day = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-day-min.nc")
+            month = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-month-min.nc")
+            year = xr.open_dataset(f"{AGG_DATA_PATH}/{variable}/{variable}-year-min.nc")
+
+            for y in years:
+                year_data = year.sel(time=f"{y}-12-31 00:00:00", latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon))
+                if(year_data["t2m"].min() <= filter_value and filter_value <= year_data["t2m"].max()).any():
+                    years_passed.append(y)
+            
+            #return years_passed
+
+            for y in years_passed:
+                ds = month.sel(
+                    time=slice(f"{y}-01-01 00:00:00", f"{y}-12-31 00:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds:
+                    ds_list.append(ds)
+
+            if ds_list:
+                ds = xr.concat(ds_list, dim="time")
+                for m in ds["time"]:
+                    month_data = ds.sel(time=m)
+                    if (month_data["t2m"].min() <= filter_value and filter_value <= month_data["t2m"].max()).any():
+                        months_passed.append(str(m.values)[:10])
+            
+            #return months_passed
+            
+            for m in months_passed:
+                ds_m = day.sel(
+                    time=slice(f"{m[:7]}-01 00:00:00", f"{m} 23:00:00"),
+                    latitude=slice(max_lat, min_lat),
+                    longitude=slice(min_lon, max_lon)
+                )
+                
+                if ds_m:
+                    ds_m_list.append(ds_m)
+
+            if ds_m_list:
+                ds_m = xr.concat(ds_m_list, dim="time")
+                for d in ds_m["time"]:
+                    day_data = ds_m.sel(time=d)
+                    #return day_data["t2m"].max()
+                    if(day_data["t2m"].max() >= filter_value and filter_value >= day_data["t2m"].min()):
+                        days_passed.append(str(d.values)[:10])
+            
+            #return days_passed
+            
+            for d in days_passed:
+                whole_year, whole_month, whole_day, ds_d = get_whole_period_between(f"{d} 00:00:00", f"{d} 23:00:00")
+
+                if ds_d:
+                    ds_d_list.append(ds_d)
+            #return ds_d_list
+ 
+            for h in ds_d_list:
+                #return h[0][:4]
+                hour_dataset = xr.open_dataset(f"{RAW_DATA_PATH}/{variable}/{variable}-{h[0][:4]}.nc")
+                for hour in h:
+                    #return h
+                    #return hour[:4]
+                    temp = hour_dataset["t2m"].sel(time=hour).values
+                    #return temp.max() #317.20237690953525
+                    #hour_temp_list.append((hour, temp.max()))
+                    #return hour_temp_list
+                    if(temp.min() == filter_value):
+                        res.append(hour)
+
+            return res
+
+            
 
 
 def find_area_baseline(
